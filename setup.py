e1b5e9340353b52df19261992c632e28a7606526
Robert Mazurowski <werfish1@gmail.com>
2023-06-28 13:12:59 +0200

    Finished the setup.py package for deployment to PyPi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Context',
-    description='Cross-platform, AI code generator CLI tool and ContexLang processor',
+    description='Cross-platform, AI code generator CLI tool and ContexLang preprocessor',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     version='0.1',
@@ -15,19 +15,20 @@ setup(
     },
     author='Robert Mazurowski',
     author_email='werfish1@gmail.com',
-    url='',
+    url='https://github.com/werfish/Context-Lang',
     project_urls={
-        'Documentation': '',
-        'Source': '',
-        'Tracker': 'https://github.com/pyglet/pyglet/issues',
+        'Documentation': 'https://github.com/werfish/Context-Lang',
+        'Source': 'https://github.com/werfish/Context-Lang',
+        'Tracker': 'https://github.com/werfish/Context-Lang/issues',
     },
-    license='BSD',
+    license='Apache 2.0',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Environment :: Win32 (MS Windows)',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: Apache Software License',  # Apache license, check later if correct
         'Operating System :: Microsoft :: Windows :: Windows 10',
+        #Here is a list of the most pupular programming languages
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Assembly',
         'Programming Language :: Basic',

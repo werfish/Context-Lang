a574d414947ae47d392444a33c172f2077231737
Robert Mazurowski <werfish1@gmail.com>
2024-03-30 15:14:39 +0100

    PRE DEMO STATE

--- test/Generator Test/API_TEST/db.py
+++ test/Generator Test/API_TEST/db.py
@@ -17,39 +17,5 @@
 
 # <context:DB_PY>
 # <CreateDatabaseConnection>
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-import os
-
-# Dependency
-from . import db_models  # assuming db_models is a module in the current package
-
-# Constants for your database setup
-DATABASE_NAME = 'database.db'
-DATABASE_URL = f'sqlite:///{DATABASE_NAME}'
-
-# Ensure the database directory exists. Create it if it doesn't.
-if not os.path.exists(DATABASE_NAME):
-    open(DATABASE_NAME, 'a').close()
-
-# Create the SQLAlchemy engine
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-
-# Create a configured "SessionLocal" class which will serve as a factory for new Session objects
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Create a base class for declarative class definitions
-Base = declarative_base()
-
-def init_db():
-    """
-    Create all tables in the database.
-    This is equivalent to "Create Database" in other SQL databases,
-    given SQLite doesn't separate the concept of database create from table create.
-    """
-    import db_models  # Import all the models
-    Base.metadata.create_all(bind=engine)
-
 # <CreateDatabaseConnection/>
 # <context:DB_PY/>

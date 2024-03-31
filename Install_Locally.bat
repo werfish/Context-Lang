22f21159e152422933b0b92aab2b7e1be9f8177a
Robert Mazurowski <werfish1@gmail.com>
2024-03-31 15:36:07 +0200

    Changed the install script and conducted a test for html css where first hmtl file is generated and then css based on it. Test Succesfull

--- Install_Locally.bat
+++ Install_Locally.bat
@@ -1,3 +1,4 @@
-cd "C:\Users\Robert\Desktop\PROJECTS\AI PROJECTS\Context\Context"
+@cd "C:\Users\Robert\Desktop\PROJECTS\AI PROJECTS\Context\Context"
+cd "C:\Users\werfi\Desktop\My Projects\Context-Lang"
 pip install -e .
 pause

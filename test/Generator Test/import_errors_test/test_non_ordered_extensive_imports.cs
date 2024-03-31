72a49bce18fde084d3ff2cf06e350a78e82b98df
Robert Mazurowski <werfish1@gmail.com>
2024-04-01 00:52:20 +0200

    Improving the test scenarios for testing the error output, and user error detections

--- test/Generator Test/import_errors_test/test_non_ordered_extensive_imports.cs
+++ test/Generator Test/import_errors_test/test_non_ordered_extensive_imports.cs
@@ -1,29 +1,21 @@
-
-// Importing specific context variables
+//<file:CODE3>test_file_8.py<file:CODE3/>
 //<import:new1>test_file_1.txt<import:new1/>
-//<import:new2>test_file_1.txt<import:new2/>
+//<file:CODE2>test_file_7.py<file:CODE2/>
 //<import:new3>test_file_1.txt<import:new3/>
-//<import:A>test_file_1.txt<import:A/>
-//<import:B>test_file_1.txt<import:B/>
-
-// Importing all context variables from a file
-//<import>test_file_2.txt<import/>
 //<import>test_file_3.txt<import/>
-
-// Importing another file as a context variable (file context)
-//<file:FILE_CONTEXT>test_file_4.txt<file:FILE_CONTEXT/>
+//<import:new2>test_file_1.txt<import:new2/>
+//<import:A>test_file_1.txt<import:A/>
+//<import>test_file_2.txt<import/>
 //<file:ANOTHER_FILE_CONTEXT>test_file_5.txt<file:ANOTHER_FILE_CONTEXT/>
-//<file:CODE1>test_file_6.txt<file:CODE1/>
-//<file:CODE2>test_file_7.txt<file:CODE2/>
-//<file:CODE3>test_file_8.txt<file:CODE3/>
+
 using System;
-namespace ContextTest
+namespace ContextTest//<import:B>test_file_1.txt<import:B/>
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Testing extensive imports with Context.");
+            Console.WriteLine("Testing extensive imports with Context."); //<file:CODE1>test_file_6.py<file:CODE1/>
         }
-    }
+    } //<file:FILE_CONTEXT>test_file_4.txt<file:FILE_CONTEXT/>
 }
